d3e258105ff8715e8e2dde174573f91ce1ab06bd
gigiftttgi <natcha_si@cmu.ac.th>
2021-01-19 15:34:25 +0700

    lab12_1_final

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,44 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[])
+{
+	double avg,sd,gm,hm,max,min,sum,sumsd=0,sumgm=1,sumhm;
+	for(int i=0;i<n;i++)
+	{
+		sum+=a[i];
+	}
+	avg=sum/n;
+	b[0]=avg;
+	for(int i=0;i<n;i++)
+	{
+		sumsd=sumsd+pow(a[i],2);
+	}
+	sd=sqrt((sumsd/n)-avg*avg);
+	b[1]=sd;
+	for(int i=0;i<n;i++)
+	{
+		sumgm=sumgm*a[i];
+	}
+	gm=pow(sumgm,(double)1/n);
+	b[2]=gm;
+ 	for(int i=0;i<n;i++)
+	{
+		sumhm=sumhm+1/a[i];
+	}
+	hm=n/sumhm;
+	b[3]=hm;
+	max = a[0];
+    min = a[0];
+    for(int i=1; i<n; i++)
+    {
+        if(a[i] > max)
+            max = a[i];
+        if(a[i] < min)
+            min = a[i];
+    }
+    b[4]=max;
+    b[5]=min;
+    
+}
