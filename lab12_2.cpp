3c4319f0713f80a76244abe976b31dbf0cb98e5c
gigiftttgi <natcha_si@cmu.ac.th>
2021-01-19 17:13:49 +0700

    lab12_2_final

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,3 +21,37 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool im[N][M])
+{
+    for(int j=0 ;j<72 ;j++)
+		{cout << "-";}
+	cout << "\n";
+	for(int j=0 ;j<N ;j++)
+		{
+		cout << "|";
+		for(int i=0 ; i<M ; i++)
+			{
+				if(im[j][i]==1)
+					cout << "*";
+				else
+					cout << " ";	
+			}
+		cout << "|\n";
+		}
+		for(int j=0 ;j<72 ;j++)
+		{cout << "-";}
+		cout << "\n";
+}
+
+void updateImage(bool im[N][M],int s,int x,int y)
+{
+	for(int j=0;j<N;j++)
+	{
+		for(int i=0;i<M;i++)
+		{
+			if(sqrt(pow(j-x,2)+pow(i-y,2)) <= s-1)
+			im[j][i]=1;
+		}
+	}
+}
